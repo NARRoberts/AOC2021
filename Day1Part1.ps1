dcbad8f26dad284b2bbdc40989670abf4e481012
NARRoberts <95424443+NARRoberts@users.noreply.github.com>
2021-12-02 13:48:25 +0000

    Day 1

---
+++ Day1Part1.ps1
@@ -0,0 +1,16 @@
+﻿$data = Get-Content .\Day1Input.txt
+
+$Depth = $data[0]
+
+$Result = 0
+
+foreach($Val in $data)
+{
+    if ($Val -gt $Depth)
+    {
+        $Result++
+    }
+    $Depth = $Val
+}
+
+$Result
---
+++ Day1Part2.ps1
@@ -0,0 +1,24 @@
+﻿$data = Get-Content .\Day1Input.txt
+
+$end = $data.Count - 2
+
+
+$current = $data[0] + $data[1] + $data[2]
+
+$Result = 0
+
+$idx = 1
+
+while ($idx -lt $end)
+{
+    $this = $data[$idx] + $data[$idx+1] + $data[$idx + 2]
+    if ($this -gt $current)
+    {
+        $Result++
+    }
+    $current = $this
+    $idx++
+}
+
+
+$Result
