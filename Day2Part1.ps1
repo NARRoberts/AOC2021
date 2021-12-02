981a800914afb671d9f702b6b4936f1e8c66a6c3
NARRoberts <95424443+NARRoberts@users.noreply.github.com>
2021-12-02 13:48:45 +0000

    Day 2

---
+++ Day2Part1.ps1
@@ -0,0 +1,32 @@
+﻿$data = Get-Content .\Day2Input.txt
+
+$parsed = @()
+
+foreach ($line in $data)
+{
+    $new = $line.Split(" ")
+    $parsed += [pscustomobject]@{Action=$new[0];Value=$new[1]}
+}
+
+$Pos = 0
+$Depth = 0
+
+foreach ($move in $parsed)
+{
+    if ($move.Action -eq "forward")
+    {
+        $Pos += $move.Value
+    }
+    elseif ($move.Action -eq "down")
+    {
+        $Depth += $move.Value
+    }
+    else
+    {
+        $Depth -= $move.Value
+    }
+}
+
+$Pos
+$Depth
+($Pos * $Depth)
---
+++ Day2Part2.ps1
@@ -0,0 +1,34 @@
+﻿$data = Get-Content .\Day2Input.txt
+
+$parsed = @()
+
+foreach ($line in $data)
+{
+    $new = $line.Split(" ")
+    $parsed += [pscustomobject]@{Action=$new[0];Value=$new[1]}
+}
+
+$Pos = 0
+$Depth = 0
+$Aim = 0
+
+foreach ($move in $parsed)
+{
+    if ($move.Action -eq "forward")
+    {
+        $Pos += $move.Value
+        $Depth += $Aim * $move.Value
+    }
+    elseif ($move.Action -eq "down")
+    {
+        $Aim += $move.Value
+    }
+    else
+    {
+        $Aim -= $move.Value
+    }
+}
+
+$Pos
+$Depth
+($Pos * $Depth)
